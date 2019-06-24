c93db422ba42bdd67ca740c64faed3013fdd4b81
o.sirota <you@example.com>
2019-06-24 13:41:06 +0300

    created project

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+
+app.set("view engine", "ejs");
+
+
+
+app.get("/", function(request, response){
+ 
+    response.render("index", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890"
+    });
+});
+
+
+app.listen(3000);
